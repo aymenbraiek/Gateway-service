7f703b532986c48fedec597df731a55bd47540e0
aymen braiek <aymen.braiek@esprit.tn>
2020-07-20 23:12:33 +0100

    how conf my gateway with publish API -closes #2

--- src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
+++ src/main/java/com/example/gatewayservice/GatewayServiceApplication.java
@@ -15,11 +15,13 @@ public class GatewayServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayServiceApplication.class, args);
 	}
-	  // @Bean
-		//configurer  spring cloud gateway statiquement pour les API public
-		RouteLocator StaticRoutes(RouteLocatorBuilder builder)
+	@Bean
+	RouteLocator StaticRoutes(RouteLocatorBuilder builder)
 	{
+		//configurer  spring cloud gateway statiquement pour les API public
 		return builder.routes()
+				//utiliser des filtres 
+				//rewritePath reecrire les url 
 				//.route(r->r.path("/customers/**").uri("lb://CUSTOMER-SERVICE").id("r1"))
 				//.route(r->r.path("/products/**").uri("lb://INVENTORY-SERVICE").id("r2"))
 				//.route(r->r.path("/biat/**").uri("lb://CUSTOMER-SERVICE").id("r3"))
@@ -27,8 +29,8 @@ public class GatewayServiceApplication {
 				.addRequestHeader("x-rapidapi-key","65638b4164msh211250a475b1409p17b586jsn8e1f2ffc3370")
 				.rewritePath("/publicscountries/(?<segment>.*)","/${segment}")
 						//lorsque api est n'est pas disponible il faux définir un retour au lieu de 404 on a utilisé hystrix
-						.hystrix(h->h.setName("countries").setFallbackUri("forward:/defaultCountries")))
-						.uri( "https://restcountries-v1.p.rapidapi.com").id("route1"))
+						//.hystrix(h->h.setName("countries").setFallbackUri("forward:/defaultCountries")))
+						.uri( "https://restcountries-v1.p.rapidapi.com/all").id("route1"))
 				.route(r->r.path("/muslim/**").filters(f->f.addRequestHeader("x-rapidapi-host","muslimsalat.p.rapidapi.com")
 						.addRequestHeader("x-rapidapi-key","65638b4164msh211250a475b1409p17b586jsn8e1f2ffc3370")
 						.rewritePath("/muslim/(?<segment>.*)","/${segment}")
